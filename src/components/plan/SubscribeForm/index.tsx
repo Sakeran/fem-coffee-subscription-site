e55b91f1c25824fbed71076e6e285a21b0b80dd1
Alexander <Sakeran@gmail.com>
2021-12-03 14:08:16 -0500

    Implement initial OptionSelector component

--- src/components/plan/SubscribeForm/index.tsx
+++ src/components/plan/SubscribeForm/index.tsx
@@ -5,6 +5,40 @@ import formData from "./formData.json";
 import { SubscribeDrawer } from "./SubscribeDrawer";
 import { OptionGroup } from "./OptionGroup";
 
+const OptionSelectorLabel: Component<{
+  id: string;
+  text: string;
+  active?: boolean;
+  completed?: boolean;
+}> = (props) => {
+
+  const styles = () => {
+    return props.active ? "" : "opacity-40";
+  }
+
+  const idStyles = () => {
+    return props.completed ? "text-primary" : "text-gray";
+  }
+
+  return (
+    <li class={`${styles()} text-6 font-serif font-black py-6 border-b border-gray`}>
+      <span class={`${idStyles()} inline-block mr-6`}>{props.id}</span> <span>{props.text}</span>
+    </li>
+  );
+};
+
+const OptionSelector: Component = () => {
+  return (
+    <ul>
+      <OptionSelectorLabel id="01" text="Preferences" completed />
+      <OptionSelectorLabel id="02" text="Bean Type" />
+      <OptionSelectorLabel id="03" text="Quantity" />
+      <OptionSelectorLabel id="04" text="Grind Option" />
+      <OptionSelectorLabel id="05" text="Deliveries" active />
+    </ul>
+  );
+};
+
 export const SubscribeForm: Component = () => {
   const handleSelection = (fieldId, option) => {
     console.log(fieldId, option);
@@ -12,6 +46,9 @@ export const SubscribeForm: Component = () => {
 
   return (
     <section>
+      <div class="hidden">
+        <OptionSelector />
+      </div>
       <div class="flex flex-col gap-24 md:gap-25 lg:gap-22">
         <For
           each={formData}
